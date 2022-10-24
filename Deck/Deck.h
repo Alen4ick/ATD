900ddf96e046cfc930684bb9f06d7141e8f67622
Alen4ick <karapetian30@mail.ru>
2022-10-24 18:31:11 +0300

    d

---
+++ Deck/Deck.h
@@ -0,0 +1,12 @@
+#include <iostream>
+template<typename T>
+class Queue {
+    public:
+    Queue() = default;
+    Queue(const Queue& queue);
+    ~Queue();
+
+    T top(const Queue& queue);
+    T pop(Queue& queue);
+    void push(Queue& queue);
+};
