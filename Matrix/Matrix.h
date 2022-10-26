f5688dfe01a0748beb3bfe36a0d3d18dbe3cbef1
Alen4ick <karapetian30@mail.ru>
2022-10-26 15:12:36 +0300

    a

---
+++ Matrix/Matrix.h
@@ -0,0 +1,17 @@
+/* #include<iostream>
+
+template<typename T>
+class Matrix{
+    private:
+    size_t i{},j{};
+    public:
+    Matrix() =default;
+    Matrix(size_t i, size_t j);
+    Matrix(const Matrix& matrix);
+    ~Matrix() = default;
+    T* operator[](size_t i, size_t j);
+    T* Size(){
+        T* m = {this->.i, this->j};
+        return m;
+    }
+}; */
---
+++ Matrix/d.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+template<typename T>
+class Matrix{
+    private:
+    size_t i{},j{};
+    public:
+    Matrix() =default;
+    Matrix(size_t i, size_t j);
+    Matrix(const Matrix& matrix);
+    ~Matrix() = default;
+    T* operator[](size_t i);
+    T* Size();
+};
